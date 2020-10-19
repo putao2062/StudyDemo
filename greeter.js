1f7022c336e721622478907726ced7f4aa70798f
chenxiaozhen <chenxiaozhen@qq.com>
2020-10-20 00:09:24 +0800

    class  类

--- greeter.js
+++ greeter.js
@@ -1,6 +1,19 @@
-// main  入口文件
 function greeter(person) {
-    return 'hello,' + person;
+    return 'hello,' + person.firstName + person.lastName;
 }
-var user = 'json';
+var Student = /** @class */ (function () {
+    function Student(firstName, middleName, lastName) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleName + " " + lastName;
+    }
+    return Student;
+}());
+// let user = 'json' 
+var user = {
+    firstName: 'chen',
+    lastName: 'json'
+};
+var studentUser = new Student('json', 'm.', 'User');
 document.body.textContent = greeter(user);
--- greeter.ts
+++ greeter.ts
@@ -1,16 +1,26 @@
 // main  入口文件
 interface Person {
   firstName: string;
-  lastName:string;
+  lastName: string;
 }
-function greeter(person:Person){
+function greeter(person: Person) {
   return 'hello,' + person.firstName + person.lastName
 }
 
+class Student {
+  fullName: string;
+  constructor(public firstName: string,
+    public middleName: string,
+    public lastName: string, ) {
+    this.fullName = firstName + " " + middleName + " " + lastName
+
+  }
+}
 // let user = 'json' 
 let user = {
-  firstName:'chen',
-  lastName:'json'
+  firstName: 'chen',
+  lastName: 'json'
 }
 
+let studentUser = new Student('json','m.','User')
 document.body.textContent = greeter(user); 
