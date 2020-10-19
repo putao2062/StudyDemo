e06492f6055f72d01a5d514f89673b81a9e48202
chenxiaozhen <chenxiaozhen@qq.com>
2020-10-19 21:53:41 +0800

    常规测试  tsc greeter.ts  编译文件

---
+++ greeter.js
@@ -0,0 +1,6 @@
+// main  入口文件
+function greeter(person) {
+    return 'hello,' + person;
+}
+var user = 'json';
+document.body.textContent = greeter(user);
---
+++ greeter.ts
@@ -0,0 +1,8 @@
+// main  入口文件
+function greeter(person){
+  return 'hello,' + person
+}
+
+let user = 'json'
+
+document.body.textContent = greeter(user);
