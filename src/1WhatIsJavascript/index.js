d7b0b14505a479c66bb8600daa49d69927df21be
chenxiaozhen <chenxiaozhen@qq.com>
2020-10-27 13:53:19 +0800

    封装console.log 输出自定义彩色文字，写个自己的log方法

---
+++ src/1WhatIsJavascript/index.js
@@ -0,0 +1,9 @@
+let number
+let name
+let rate = 1.2
+const greeting = "Hello,world" 
+let flag = false
+
+let {log} = require('../utils/log.js')
+log(greeting,{font:"red",bg:"green"})
+console.log(greeting)
