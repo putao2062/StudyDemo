d1c094a86295ce93759d1ae2bf9bd0a7e2937c9b
chenxiaozhen <chenxiaozhen@qq.com>
2020-10-27 18:10:32 +0800

    javascript 中的算数运算和数学库函数

--- src/1WhatIsJavascript/index.js
+++ src/1WhatIsJavascript/index.js
@@ -1,16 +1,29 @@
 let number
 let name
 let rate = 1.2
-const greeting = "Hello,world" 
+const greeting = "Hello,world"
 let flag = false
 
-let {log} = require('../utils/log.js')
+let { log } = require('../utils/log.js')
 let bright = log.style.bright
 let article = log.style.article
 let grass = log.style.grass
 let flower = log.style.flower
 
-log(greeting,bright)
-log(greeting,article)
-log(greeting,grass)
-log(greeting,flower)
+log(greeting, bright)
+log(greeting, article)
+log(greeting, grass)
+log(greeting, flower)
+
+// javascript 中的算数运算和数学库函数
+let x = 3
+let y = 1.1
+log(x+y)
+log(x-y)
+log(x*y)
+log(x/y)
+log((x+y)*(x-y))
+
+let z = 9
+log(Math.sqrt(z))
+log(Math.abs(y/x))
