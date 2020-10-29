ba8447a1579d759a19282a08c4dbc1461f1732e0
chenxiaozhen <chenxiaozhen@qq.com>
2020-10-29 14:22:55 +0800

    3List类nodejs命令行小应用实践结果

--- src/3List/index.js
+++ src/3List/index.js
@@ -128,9 +128,11 @@ function length () {
 
 // 判断函数
 function contains (element) {
-  for (let i = 0, len = this.dataStore; i < len; ++i) {
-    if (this.dataStore[i] == element)
+  // 容易出错的地方，没点出length属性，导致判断失效
+  for (let i = 0, len = this.dataStore.length; i < len; ++i) {
+    if (this.dataStore[i] == element) {
       return true
+    }
   }
   return false
 }
@@ -151,11 +153,117 @@ log(names.toString(), flower)
 
 // 使用迭代器访问列表
 
-for(names.front();names.currPos()< names.length();names.next()){
-   log(names.currPos())
-   log(names.length())
-   log(names.getElement(),article)
-   
-   // 因为next函数中的pos值永远都<= names.listSize-1，所以这个循环判断的条件会永远为真 ,添加一个修复的终止循环的代码
-   if(names.currPos() == names.length() -1) break
+for (names.front(); names.currPos() < names.length(); names.next()) {
+  log(names.currPos())
+  log(names.length())
+  log(names.getElement(), bright)
+
+  // 因为next函数中的pos值永远都<= names.listSize-1，所以这个循环判断的条件会永远为真 ,添加一个修复的终止循环的代码
+  if (names.currPos() == names.length() - 1) break
+}
+
+// 一个基于列表的应用
+let fs = require('fs');
+let persons = []
+
+
+function createArr () {
+  return new Promise((resolve, reject) => {
+    // fs.readFile('/study/Github/StudyDemo/src/3List/persons.txt', 'utf8', (err, data) => {
+    fs.readFile(__dirname + '/persons.txt', 'utf8', (err, data) => {
+      // log(data)
+      if (err) {
+        reject(['读取文件失败'])
+      } else {
+        persons = data.split('\n').map(item => {
+          return item.trim()
+        })
+        resolve(creatPersonListObj(persons))
+      }
+    })
+  })
+}
+function creatPersonListObj (persons) {
+  let personsList = new List()
+  for (let i = 0, len = persons.length; i < len; ++i) {
+    personsList.append(persons[i])
   }
+  return personsList
+}
+
+let personsList = null
+async function displayList () {
+  let list = personsList === null ? await createArr() : personsList
+  personsList = list
+
+  log('看看都有哪些人', article)
+
+  for (list.front(); list.currPos() < list.length(); list.next()) {
+    let item =  list.getElement() 
+    if (item instanceof Customer) {
+      log(item.name + "约走了" + item.person)
+    } else {
+      log(item)
+    }
+    if (list.currPos() == list.length() - 1) break
+  }
+
+}
+
+// 创建一个客户对象
+function Customer (name, person) {
+  this.name = name
+  this.person = person
+}
+let customersList = new List()
+
+function checkOut (name, person, personsList, customersList) {
+  if (personsList.contains(person)) {
+    let c = new Customer(name, person)
+    customersList.append(c)
+    personsList.remove(person)
+    personsList.append(c)
+    log(person + "星起天会和" + name + "你约会", flower)
+  } else {
+    log(person + "已经被约走了", grass)
+  }
+
+  // rl.close()
+  // rl.pause()
+}
+
+// 命令行应用
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+function getCustomerName () {
+  return new Promise((resolve, reject) => {
+    rl.question('请输入你的名字', (anwser) => {
+      resolve(anwser)
+    })
+  })
+}
+
+
+function getPersonName () {
+  displayList()
+  return new Promise((resolve, reject) => {
+    rl.question('你想约谁去看电影?请输入对方的名字', (anwser) => {
+      resolve(anwser)
+    })
+  })
+}
+
+async function app (goOn) {
+  // rl.resume()
+  let userName = await getCustomerName()
+  let personName = await getPersonName()
+  checkOut(userName, personName, personsList, customersList)
+  goOn ? app() : rl.pause()
+}
+
+app(true)
