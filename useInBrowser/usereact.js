993949cfe602e19438879364c478ae96814f66eb
chenxiaozhen <chenxiaozhen@qq.com>
2021-02-10 21:26:01 +0800

    修改单词

--- useInBrowser/usereact.js
+++ useInBrowser/usereact.js
@@ -1,7 +1,7 @@
 /*
  * @Author: XiaozhenChen
  * @Date: 2021-02-10 20:51:36
- * @LastEditTime: 2021-02-10 21:21:48
+ * @LastEditTime: 2021-02-10 21:25:40
  * @LastEditors: Please set LastEditors
  * @Description: 
  * @FilePath: \StudyDemo\useInBrowser\usereact.js
@@ -13,7 +13,7 @@ ReactDOM.render('react coming',document.getElementById('react-container'))
 //   const name = 'cxz'
 //   const element = <h1>Hello, world!{name}</h1>
 //   ReactDOM.render(
-//     // <h1>Hello, world!</,h1>,   //在浏览器中 会报语法错Uncaught SyntaxError: Unexpected token '<'Uncaught SyntaxError: Unexpected token '<',需要使用bable库
+//     // <h1>Hello, world!</,h1>,   //在浏览器中 会报语法错Uncaught SyntaxError: Unexpected token '<'Uncaught SyntaxError: Unexpected token '<',需要使用babel库
 //     // 'Hello, world!',
 //     element,
 //     document.getElementById('hello_world')
